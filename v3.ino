b1d0ebf08fc9f8b497cc136769f976b1ea7ae0f1
EtherliaS <49315934+EtherliaS@users.noreply.github.com>
2023-05-25 20:02:02 +0300

    Update v3.ino
    ddddd

--- v3.ino
+++ v3.ino
@@ -1,12 +1,14 @@
 #include <Keyboard.h>
 
+long releaseDebounce = 20;
+
 unsigned long time_activation_z = 0;
 unsigned long time_activation_x = 0;
 
 bool xPressed = false;
 bool zPressed = false;
 //Z 484-435 (~50)
-//X 497-474 (~23 -- shit)
+//X 497-474 (~24 -- shit)
 
 int pr_z = 0;
 int pr_x = 0;
@@ -19,27 +21,29 @@ void setup() {
   Serial.begin(9600);
   Keyboard.begin();
 }
-
 void loop() {
   while(1){
     currentTime = micros();
-    if(currentTime % 500 < 50){
+    if(currentTime % 500 < 20){
       checkPins();
     }
   }
 }
 
 void checkPins(){
+  
+  zState = analogRead(A1);
+  zState = analogRead(A1);
+  zState = analogRead(A1);
   zState = analogRead(A1);
   xState = analogRead(A2);
   pr_z = previous_point_z;
   pr_x = previous_point_x;
   previous_point_z = zState;
   previous_point_x = xState;
-  
   //Z
   if(zState < 475){
-    if (!zPressed && zState < previous_point_z+2 && previous_point_z < pr_z+2 && millis() - time_activation_z > 0.2)
+    if (!zPressed && zState < previous_point_z+2 && previous_point_z < pr_z+2 && millis() - time_activation_z > 80)
     {
         time_activation_z = millis();
         pressZ();
@@ -51,8 +55,8 @@ void checkPins(){
   else releaseZ();
 
   //X
-  if(xState < 492){
-    if (!xPressed && xState < previous_point_x+2 && previous_point_x < pr_x+2 && millis() - time_activation_x > 0.2)
+  if(xState < 490){
+    if (!xPressed && xState < previous_point_x+2 && previous_point_x < pr_x+2 && millis() - time_activation_x > 80)
     {
         time_activation_x = millis();
         pressX();
